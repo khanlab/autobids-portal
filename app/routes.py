3f1bb4ec33b42dfa5f57a59b6ac6c51a05499b6e
Samantha Bartman <samanthabartman@Samanthas-MBP.home>
2021-05-05 09:44:10 -0400

    First commit

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db 
 from app.forms import LoginForm, BidsForm, RegistrationForm
-from app.models import User 
+from app.models import User
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
