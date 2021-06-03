34091a96c1c79f8891bf94369c4fbc69f12eb194
Samantha Bartman <samanthabartman@gmail.com>
2021-06-03 15:10:39 -0400

    Updates to Login, Register and Survey test cases

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -8,13 +8,16 @@ def test_login_page(test_client):
 
 def test_valid_login_logout(test_client, init_database):
     response = test_client.post('/login',
-                                data=dict(email='johnsmith@gmail.com', password='Password123'),
+                                data=dict(email='johnsmith@gmail.com', 
+                                password='Password123',
+                                submit='Sign In'),
                                 follow_redirects=True)
     assert response.status_code == 200
     assert b'Logout' in response.data
     assert b'Login' not in response.data
-    assert b'New study request: autobids-cfmm' in response.data
+    assert b'Name' in response.data
     assert b'Results' in response.data
+    assert b'Invalid email or password' not in response.data
 
     response = test_client.get('/logout', follow_redirects=True)
     assert response.status_code == 200
@@ -24,63 +27,79 @@ def test_valid_login_logout(test_client, init_database):
 
 def test_invalid_login(test_client, init_database):
     response = test_client.post('/login',
-                                data=dict(email='johnsmith@gmail.com', password='Password12345'),
+                                data=dict(email='johnsmith@gmail.com', 
+                                password='Password12345',
+                                submit='Sign In'),
                                 follow_redirects=True)
     assert response.status_code == 200
     assert b'Logout' not in response.data
     assert b'Login' in response.data
     assert b'Click to Register!' in response.data
+    assert b'Name' not in response.data
     assert b'Results' not in response.data
+    assert b'Invalid email or password' in response.data
 
 
 def test_login_already_logged_in(test_client, init_database, login_default_user):
     response = test_client.post('/login',
-                                data=dict(email='johnsmith@gmail.com', password='Password123'),
+                                data=dict(email='johnsmith@gmail.com', 
+                                password='Password123',
+                                submit='Sign In'),
                                 follow_redirects=True)
+    print(response.data)
     assert response.status_code == 200
     assert b'Logout' in response.data
     assert b'Login' not in response.data
     assert b'Click to Register!' not in response.data
+    assert b'Name' in response.data
     assert b'Results' in response.data
 
 def test_valid_registration(test_client, init_database):
     response = test_client.post('/register',
                                 data=dict(email='johndoe@yahoo.com',
-                                          password='Password123',
-                                          confirm='Password123'),
+                                    password='Password123',
+                                    password2='Password123',
+                                    submit='Register'),
                                 follow_redirects=True)
     assert response.status_code == 200
-    print(response.data)
     assert b'Logout' not in response.data
     assert b'Login' in response.data
+    assert b'Click to Register!' in response.data
+    assert b'Congratulations, you are now a registered user!' in response.data
+    assert b'Name' not in response.data
     assert b'Results' not in response.data
 
 def test_invalid_registration(test_client, init_database):
     response = test_client.post('/register',
                                 data=dict(email='johnsmith@hotmail.com',
-                                          password='Password123',
-                                          password2='Password1234'),   
+                                    password='Password123',
+                                    password2='Password1234',
+                                    submit='Register'),   
                                 follow_redirects=True)
     assert response.status_code == 200
-    assert b'[This field is required.]' not in response.data
+    assert b'Field must be equal to password.' in response.data
     assert b'Logout' not in response.data
     assert b'Login' in response.data
     assert b'Register' in response.data
+    assert b'Congratulations, you are now a registered user!' not in response.data
     assert b'Results' not in response.data
 
 def test_duplicate_registration(test_client, init_database):
     test_client.post('/register',
                      data=dict(email='johnappleseed@gmail.com',
-                               password='Password1234',
-                               confirm='Password1234'),
+                        password='Password1234',
+                        password2='Password1234',
+                        submit='Register'),
                      follow_redirects=True)
     
     response = test_client.post('/register',
                                 data=dict(email='johnappleseed@gmail.com',
                                     password='Password12345',
-                                    confirm='Password12345'),
+                                    password2='Password12345',
+                                    submit='Register'),
                                 follow_redirects=True)
     assert response.status_code == 200
+    assert b'There is already an account using this email address. Please use a different email address.' in response.data
     assert b'Logout' not in response.data
     assert b'Login' in response.data
     assert b'Register' in response.data
@@ -88,12 +107,14 @@ def test_duplicate_registration(test_client, init_database):
 
 def test_valid_login_complete_survey_logout(test_client, init_database):
     response = test_client.post('/login',
-                                data=dict(email='johnsmith@gmail.com', password='Password123'),
+                                data=dict(email='johnsmith@gmail.com', 
+                                    password='Password123',
+                                    submit='Sign In'),
                                 follow_redirects=True)
     assert response.status_code == 200
     assert b'Logout' in response.data
     assert b'Login' not in response.data
-    assert b'New study request: autobids-cfmm' in response.data
+    assert b'Name' in response.data
     assert b'Results' in response.data
 
     response = test_client.post('/index',
@@ -101,8 +122,8 @@ def test_valid_login_complete_survey_logout(test_client, init_database):
                                     email = 'johnsmith@gmail.com',
                                     status = 'undergraduate',
                                     scanner = 'type2',
-                                    scan_number = 4,
-                                    study_type = True,
+                                    scan_number = '4',
+                                    study_type = 'y',
                                     familiarity_bids = '1',
                                     familiarity_bidsapp = '1',
                                     familiarity_python = '1',
@@ -114,24 +135,27 @@ def test_valid_login_complete_survey_logout(test_client, init_database):
                                     principal = 'Khan',
                                     project_name = 'Autobids',
                                     dataset_name = '',
-                                    sample = datetime.datetime(2021, 1, 10, 0, 0),
+                                    sample = '2021-01-10',
                                     retrospective_data = True,
-                                    retrospective_start = datetime.datetime(2021, 1, 1, 0, 0),
-                                    retrospective_end = datetime.datetime(2021, 1, 5, 0, 0),
-                                    consent = True,
+                                    retrospective_start = '2021-01-01',
+                                    retrospective_end = '2021-01-05',
+                                    consent = 'y',
                                     comment = '',
-                                    submission_date = datetime.datetime(2021, 1, 1, 10, 10, 10, 100000)),
-                                follow_redirects=True)
+                                    submit = 'Submit'), 
+                                    follow_redirects=True)
     assert response.status_code == 200
     assert b'Logout' in response.data
     assert b'Login' not in response.data
-    assert b'New study request: autobids-cfmm' in response.data
+    assert b'Name' in response.data
+    assert b'johnsmith@gmail.com' not in response.data
+    assert b'Thanks, the survey has been submitted!' in response.data
     assert b'Results' in response.data
 
     response = test_client.get('/logout', follow_redirects=True)
     assert response.status_code == 200
     assert b'Logout' not in response.data
     assert b'Login' in response.data
+    assert b'Name' in response.data
     assert b'Results' not in response.data
 
 def test_valid_survey(test_client, init_database):
@@ -140,8 +164,8 @@ def test_valid_survey(test_client, init_database):
                                     email = 'johnsmith@gmail.com',
                                     status = 'undergraduate',
                                     scanner = 'type2',
-                                    scan_number = 4,
-                                    study_type = True,
+                                    scan_number = '4',
+                                    study_type = 'y',
                                     familiarity_bids = '1',
                                     familiarity_bidsapp = '1',
                                     familiarity_python = '1',
@@ -153,29 +177,30 @@ def test_valid_survey(test_client, init_database):
                                     principal = 'Khan',
                                     project_name = 'Autobids',
                                     dataset_name = '',
-                                    sample = datetime.datetime(2021, 1, 10, 0, 0),
+                                    sample = '2021-01-10',
                                     retrospective_data = True,
-                                    retrospective_start = datetime.datetime(2021, 1, 1, 0, 0),
-                                    retrospective_end = datetime.datetime(2021, 1, 5, 0, 0),
-                                    consent = True,
+                                    retrospective_start = '2021-01-01',
+                                    retrospective_end = '2021-01-05',
+                                    consent = 'y',
                                     comment = '',
-                                    submission_date = datetime.datetime(2021, 1, 1, 10, 10, 10, 100000)),
+                                    submit = 'Submit'), 
                                 follow_redirects=True)
-    print(response.data)
-    assert response.status_code == 400
+    assert response.status_code == 200
     assert b'Logout' not in response.data
     assert b'Login' in response.data
-    assert b'New study request: autobids-cfmm' in response.data
+    assert b'Name' in response.data
+    assert b'johnsmith@gmail.com' not in response.data
+    assert b'Thanks, the survey has been submitted!' in response.data
     assert b'Results' not in response.data
-
+    
 def test_invalid_survey(test_client, init_database):
     response = test_client.post('/index',
                                 data=dict(name = '',
                                     email = 'johnsmith@gmail.com',
                                     status = 'undergraduate',
                                     scanner = 'type2',
-                                    scan_number = 4,
-                                    study_type = True,
+                                    scan_number = '4',
+                                    study_type = 'y',
                                     familiarity_bids = '1',
                                     familiarity_bidsapp = '1',
                                     familiarity_python = '1',
@@ -187,17 +212,18 @@ def test_invalid_survey(test_client, init_database):
                                     principal = 'Khan',
                                     project_name = 'Autobids',
                                     dataset_name = '',
-                                    sample = datetime.datetime(2021, 1, 10, 0, 0),
+                                    sample = '2021-01-10',
                                     retrospective_data = True,
-                                    retrospective_start = datetime.datetime(2021, 1, 1, 0, 0),
-                                    retrospective_end = datetime.datetime(2021, 1, 5, 0, 0),
-                                    consent = True,
+                                    retrospective_start = '2021-01-01',
+                                    retrospective_end = '2021-01-05',
+                                    consent = 'y',
                                     comment = '',
-                                    submission_date = datetime.datetime(2021, 1, 1, 10, 10, 10, 100000)),
+                                    submit = 'Submit'), 
                                 follow_redirects=True)
-    print(response.data)
-    assert response.status_code == 500
+    assert response.status_code == 200
     assert b'Logout' not in response.data
     assert b'Login' in response.data
-    assert b'New study request: autobids-cfmm' in response.data
+    assert b'Name' in response.data
+    assert b'johnsmith@gmail.com' in response.data
+    assert b'Thanks, the survey has been submitted!' not in response.data
     assert b'Results' not in response.data
