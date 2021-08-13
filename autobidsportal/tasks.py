5b5c7fcd3cab5065657fae53053b4e31984c1e1f
Samantha Bartman <samanthabartman@gmail.com>
2021-08-13 11:22:09 -0400

    updates to file path for tar2bids

--- autobidsportal/tasks.py
+++ autobidsportal/tasks.py
@@ -86,14 +86,19 @@ def get_new_cfmm2tar_results(study_info, data, button_id):
 def get_info_from_tar2bids(user_id, button_id, tar_file_id):
     _set_task_progress(0, "None")
     user = User.query.get(user_id)
+    submitter_answer = db.session.query(Answer).filter(Answer.submitter_id==button_id)[0]
+    if submitter_answer.principal_other != '':
+        study_info = f"{submitter_answer.principal_other}^{submitter_answer.project_name}"
+    else:
+        study_info = f"{submitter_answer.principal}^{submitter_answer.project_name}"
     tar_file = Cfmm2tar.query.filter_by(id = tar_file_id)[0].tar_file
     prefix = app.config["TAR2BIDS_DOWNLOAD_DIR"]
-    data = "%s/%s/%s" % (prefix, button_id, tar_file_id)
+    data = "%s/%s/%s" % (prefix, study_info, button_id)
     if os.path.isdir(data) != True:
         os.makedirs(data)
     try:
         tar2bids_results = gen_utils().run_tar2bids(output_dir=data, tar_files=[tar_file])
-        tar2bids = Tar2bids(user_id=user_id, tar_file_id=tar_file_id, task_button_id=button_id, tar_file=tar_file, bids_file=tar2bids)
+        tar2bids = Tar2bids(user_id=user_id, tar_file_id=tar_file_id, task_button_id=button_id, tar_file=tar_file, bids_file=tar2bids_results)
         db.session.add(tar2bids)
         db.session.commit()
         time.sleep(10)
