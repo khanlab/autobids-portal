ea82f074bf5476b0d98c1a405094afc00e48ce24
Tristan Kuehn <tristankuehn@gmail.com>
2021-08-25 16:18:10 -0400

    Update error handling properly

--- autobidsportal/__init__.py
+++ autobidsportal/__init__.py
@@ -12,10 +12,13 @@ import rq
 
 from autobidsportal.routes import portal_blueprint, mail
 from autobidsportal.models import db, login
+from autobidsportal.errors import not_found_error, internal_error
 
 app = Flask(__name__)
 app.config.from_object(os.environ["AUTOBIDSPORTAL_CONFIG"])
 app.register_blueprint(portal_blueprint, url_prefix="/")
+app.register_error_handler(404, not_found_error)
+app.register_error_handler(500, internal_error)
 db.init_app(app)
 excel.init_excel(app)
 convention = {
@@ -33,6 +36,3 @@ login.init_app(app)
 login.login_view = "login"
 mail.init_app(app)
 bootstrap = Bootstrap(app)
-
-# Will be fixed by an application factory
-from autobidsportal import errors  # pylint: disable=C0413
--- autobidsportal/errors.py
+++ autobidsportal/errors.py
@@ -2,16 +2,14 @@
 
 from flask import render_template
 
-from autobidsportal import app, db
+from autobidsportal.models import db
 
 
-@app.errorhandler(404)
 def not_found_error(_):
     """404 error template."""
     return render_template("404.html"), 404
 
 
-@app.errorhandler(500)
 def internal_error(_):
     """500 error template."""
     db.session.rollback()
