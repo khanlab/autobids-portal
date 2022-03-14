c975faed995d337ca6df7862de57d88a5f704a52
Tristan Kuehn <tristankuehn@gmail.com>
2022-03-14 16:59:29 -0400

    Use parcel instead of nwb to fix vulnerabilities

--- autobidsportal/static/lib/autobids-react/src/index.js
+++ autobidsportal/static/lib/autobids-react/src/index.js
@@ -1,4 +1,11 @@
 import { Accordion, TextItem, DirItem, genAccordion } from "./Accordion";
+import ReactDOM from 'react-dom';
 
-export { Accordion, TextItem, DirItem, genAccordion };
-export default { Accordion, TextItem, DirItem, genAccordion };
+globalThis.Accordion = Accordion;
+globalThis.TextItem = TextItem;
+globalThis.DirItem = DirItem;
+globalThis.genAccordion = genAccordion;
+globalThis.ReactDOM = ReactDOM;
+
+export { Accordion, TextItem, DirItem, genAccordion, ReactDOM };
+export default { Accordion, TextItem, DirItem, genAccordion, ReactDOM };
