72441cc981104e8b995da3648e22840a2fbef779
Tristan Kuehn <tkuehn@uwo.ca>
2022-03-17 13:44:24 -0400

    Sort entries in file trees

--- autobidsportal/static/lib/autobids-react/src/Accordion/genAccordion.js
+++ autobidsportal/static/lib/autobids-react/src/Accordion/genAccordion.js
@@ -8,9 +8,11 @@ function genAccordion(fileTree) {
   const textItems = fileTree.files.map((fileName) => (
     <TextItem key={fileName} text={fileName}></TextItem>
   ));
+  textItems.sort((a, b) => a.text < b.text ? -1 : 1);
   const dirItems = Object.entries(fileTree.dirs).map(([key, value]) =>
     genDirItem(key, value)
   );
+  dirItems.sort((a, b) => a.text < b.text ? -1 : 1);
   return (
     <Accordion>
       {textItems}
@@ -23,9 +25,11 @@ function genDirItem(dirName, dirContents) {
   const textItems = dirContents.files.map((fileName) => (
     <TextItem key={fileName} text={fileName}></TextItem>
   ));
+  textItems.sort((a, b) => a.text < b.text ? -1 : 1);
   const dirItems = Object.entries(dirContents.dirs).map(([key, value]) =>
     genDirItem(key, value)
   );
+  dirItems.sort((a, b) => a.text < b.text ? -1 : 1);
   return (
     <DirItem text={dirName} key={dirName} dirId={"dir" + dirName.replaceAll("/", "")}>
       {textItems}
